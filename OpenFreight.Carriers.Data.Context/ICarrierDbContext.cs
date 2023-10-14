1fb353ac344d4d9c8473058782a9662fe1ee65cf
christopher schoeder <cschoede@gmail.com>
2023-10-13 19:59:27 -0500

    Move OpenFreight.Carriers.Data.Abstract to OpenFreight.Carriers.Data.Context

--- OpenFreight.Carriers.Data.Context/ICarrierDbContext.cs
+++ OpenFreight.Carriers.Data.Context/ICarrierDbContext.cs
@@ -1,12 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using OpenFreight.Carriers.Models;
 
-namespace OpenFreight.Carriers.Data.Abstract
+namespace OpenFreight.Carriers.Data.Context
 {
     /// <summary>
     /// Represents interface for fetching database records using entity framework core. 
     /// </summary>
-    public interface ICarrierDbContext 
+    public class CarrierDbContext : DbContext 
     {
         public DbSet<CarrierModel> Carriers { get; set; }
         public DbSet<CarrierContactModel> CarrierServices { get; set; }
