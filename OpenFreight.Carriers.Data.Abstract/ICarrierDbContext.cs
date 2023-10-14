cb0848ab8c69a5e8c60b7f994680b9135425f79e
christopher schoeder <cschoede@gmail.com>
2023-10-13 19:52:45 -0500

    create abstract carrier db context

--- OpenFreight.Carriers.Data.Abstract/ICarrierDbContext.cs
+++ OpenFreight.Carriers.Data.Abstract/ICarrierDbContext.cs
@@ -6,19 +6,11 @@ namespace OpenFreight.Carriers.Data.Abstract
     /// <summary>
     /// Represents interface for fetching database records using entity framework core. 
     /// </summary>
-    /// <example>
-    /// using OpenFreight.Carriers.Data.Abstract;
-    /// using OpenFreight.Carriers.Models;
-    /// 
-    /// using ContosoUniversity.Data;
-    /// using Microsoft.EntityFrameworkCore;
-    /// var builder = WebApplication.CreateBuilder(args);
-    ///
-    /// builder.Services.AddRazorPages();
-builder.Services.AddDbContext<SchoolContext>(options =>
-  options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext")));
     public interface ICarrierDbContext 
     {
         public DbSet<CarrierModel> Carriers { get; set; }
+        public DbSet<CarrierContactModel> CarrierServices { get; set; }
+        public DbSet<CarrierIdentifyingCode> IdentifyingCodes { get; set; }
+        public DbSet<CarrierInsuranceModel> CarrierInsurance { get; set; }
     }
 }
